28d4ab6e7ef1d19b5ccd40653d8bfa9a71695b08
eladshamailov <eladsham@post.bgu.ac.il>
2016-12-31 15:13:02 +0200

    implemented tools

--- src/main/java/bgu/spl/a2/sim/tools/NextPrimeHammer.java
+++ src/main/java/bgu/spl/a2/sim/tools/NextPrimeHammer.java
@@ -2,17 +2,25 @@ package bgu.spl.a2.sim.tools;
 
 import bgu.spl.a2.sim.Product;
 
+import java.math.BigInteger;
+
 /**
  * Created by win10 on 31-Dec-16.
  */
 public class NextPrimeHammer implements Tool {
     @Override
     public String getType() {
-        return null;
+        return "np-hammer";
     }
 
     @Override
     public long useOn(Product p) {
-        return 0;
+        long ans=0;
+        for(Product product:p.getParts()){
+            BigInteger primeNum=BigInteger.valueOf(product.getFinalId());
+            primeNum=primeNum.nextProbablePrime();
+            ans=ans+Math.abs(primeNum.longValue());
+        }
+        return ans;
     }
 }
