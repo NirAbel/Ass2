1e8022d340f5ded37021a068ca740fcca3972355
eladshamailov <eladsham@post.bgu.ac.il>
2016-12-31 13:07:09 +0200

    started working on part two

---
+++ src/main/java/bgu/spl/a2/sim/tools/NextPrimeHammer.java
@@ -0,0 +1,18 @@
+package bgu.spl.a2.sim.tools;
+
+import bgu.spl.a2.sim.Product;
+
+/**
+ * Created by win10 on 31-Dec-16.
+ */
+public class NextPrimeHammer implements Tool {
+    @Override
+    public String getType() {
+        return null;
+    }
+
+    @Override
+    public long useOn(Product p) {
+        return 0;
+    }
+}
