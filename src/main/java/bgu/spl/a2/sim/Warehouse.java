e2d2a76b1e77bfd668cde6134e753210c13fa867
eladshamailov <eladsham@post.bgu.ac.il>
2017-01-01 20:08:08 +0200

    added functions in warehouse

--- src/main/java/bgu/spl/a2/sim/Warehouse.java
+++ src/main/java/bgu/spl/a2/sim/Warehouse.java
@@ -1,8 +1,14 @@
 package bgu.spl.a2.sim;
 
+import bgu.spl.a2.sim.tools.GcdScrewDriver;
+import bgu.spl.a2.sim.tools.NextPrimeHammer;
+import bgu.spl.a2.sim.tools.RandomSumPliers;
 import bgu.spl.a2.sim.tools.Tool;
 import bgu.spl.a2.sim.conf.ManufactoringPlan;
 import bgu.spl.a2.Deferred;
+
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * A class representing the warehouse in your simulation
@@ -14,11 +20,24 @@ import bgu.spl.a2.Deferred;
  *
  */
 public class Warehouse {
+    private ConcurrentLinkedDeque <GcdScrewDriver> gsDrivers;
+    private ConcurrentLinkedDeque<NextPrimeHammer> npHammers;
+    private ConcurrentLinkedDeque<RandomSumPliers> rsPliers;
+    private ConcurrentLinkedDeque<Deferred<Tool>> gsdDeferreds;
+    private ConcurrentLinkedDeque<Deferred<Tool>> npDeferreds;
+    private ConcurrentLinkedDeque<Deferred<Tool>> rsDeferreds;
 
 	/**
 	* Constructor
 	*/
-    public Warehouse();
+    public Warehouse(){
+        gsDrivers=new ConcurrentLinkedDeque<>();
+        npHammers=new ConcurrentLinkedDeque<>();
+        rsPliers=new ConcurrentLinkedDeque<>();
+        gsdDeferreds=new ConcurrentLinkedDeque<>();
+        npDeferreds=new ConcurrentLinkedDeque<>();
+        rsDeferreds=new ConcurrentLinkedDeque<>();
+    }
 
 	/**
 	* Tool acquisition procedure
@@ -26,7 +45,45 @@ public class Warehouse {
 	* @param type - string describing the required tool
 	* @return a deferred promise for the  requested tool
 	*/
-    public Deferred<Tool> acquireTool(String type);
+    public Deferred<Tool> acquireTool(String type){
+        Deferred<Tool> toolDeferred= new Deferred<Tool>();
+        switch (type) {
+            case "rs-pliers":{
+                Tool t = rsPliers.poll();
+                if(t!=null){
+                    toolDeferred.resolve(t);
+                }
+                else{
+                    rsDeferreds.add(toolDeferred);
+                }
+                return toolDeferred;
+            }
+
+            case "np-hammer":{
+                Tool t = npHammers.poll();
+                if(t!=null){
+                    toolDeferred.resolve(t);
+                }
+                else{
+                    npDeferreds.add(toolDeferred);
+                }
+                return toolDeferred;
+            }
+
+            case "gs-driver":{
+                Tool t = gsDrivers.poll();
+                if(t!=null){
+                    toolDeferred.resolve(t);
+                }
+                else{
+                    gsdDeferreds.add(toolDeferred);
+                }
+                return toolDeferred;
+            }
+            default:
+                throw new NoSuchElementException("no such tools");
+        }
+    }
 
 	/**
 	* Tool return procedure - releases a tool which becomes available in the warehouse upon completion.
@@ -53,6 +110,26 @@ public class Warehouse {
 	* @param tool - type of tool to be stored
 	* @param qty - amount of tools of type tool to be stored
 	*/
-    public void addTool(Tool tool, int qty);
+    public void addTool(Tool tool, int qty){
+        switch (tool.getType()){
+            case "gs-driver":
+                for (int i=0;i<qty;i++){
+                gsDrivers.add(new GcdScrewDriver());
+            }
+                break;
+            case "np-hammer":
+                for (int i=0;i<qty;i++) {
+                    npHammers.add(new NextPrimeHammer());
+                }
+                break;
+            case "rs-pliers":
+                for (int i=0;i<qty;i++) {
+                    rsPliers.add(new RandomSumPliers());
+                }
+                break;
+            default:
+                throw new NoSuchElementException("no such tools");
+        }
+    }
 
 }
